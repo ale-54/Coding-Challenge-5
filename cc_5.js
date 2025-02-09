7b9e59069f5f5501af5a9bf1f84c707ea3416328
Ashley Le <ashleyle@usf.edu>
2025-02-09 16:55:41 -0500

    Task 2 - Order Details

--- cc_5.js
+++ cc_5.js
@@ -6,3 +6,16 @@ console.log(`Name: ${customer.name}`);
 console.log(`Age: ${customer.age}`);
 console.log(`Email: ${customer.email}`);
 //logged all customer info for displaying (name, age, email)
+
+//Task 2: Object Details
+const order = {
+    orderId: 12345, totalAmount: 150, status: "Processing",
+ //order details
+displayOrder() {
+    console.log
+    (`Order ID: ${this.orderId}`,
+    `Total Amount: $${this.totalAmount}`,
+    `Order Status: ${this.status}`)
+    } //logging the order details and separated in each line
+};
+order.displayOrder(); //displaying the order
